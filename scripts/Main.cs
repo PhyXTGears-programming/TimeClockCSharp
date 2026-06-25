baebacb5167e5a131b2e81bc5c395b51b3e1ccc7
westothemagnificent <148817895+westothemagnificent@users.noreply.github.com>
2026-06-24 22:18:30 -0400

    Fix crash loop from me not commiting stuff correctly
    - Weston J

--- scripts/Main.cs
+++ scripts/Main.cs
@@ -1,34 +1,4 @@
 using Godot;
 using System;
 
-public partial class Main : Control {
-
-    // Vars
-
-    private PackedScene notificationScene;
-
-    private VBoxContainer errorPopupContainer;
-
-    // Godot functions
-
-    public override void _Ready() {
-        notificationScene = ResourceLoader.Load<PackedScene>("res://scenes/notificationPopup.tscn");
-        errorPopupContainer = GetNode<VBoxContainer>("errorPopupContainer");
-
-        Globals.debugger.messageLogged += showNotification;
-
-        // Testing debugger
-        // GD.PushError("A");
-        // GD.PrintErr("B");
-        // GD.PushWarning("C");
-        // GD.Print("D");
-    }
-
-    // Functions
-
-    public void showNotification(string message, Debugger.LogType logType) {
-        Control notification = NotificationPopup.instantiateWithArgs(notificationScene, message, logType);
-
-        errorPopupContainer.AddChild(notification);
-    }
-}
+public partial class Main : Control {}
